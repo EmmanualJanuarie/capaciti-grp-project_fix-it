952f2ef4d1c421e07bb12773fb85fc7f7bfc4983
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2025-02-19 15:55:42 +0200

    Update services-page.js

--- App/services-page/services-page.js
+++ App/services-page/services-page.js
@@ -3,11 +3,11 @@
 // event listener for category section to toggle section
 document.getElementById('close_btn').addEventListener('click', () => {
     document.getElementById('categories_section').style.display = "none";
-    document.getElementById('fixit_inquiry_section').style.display = "display";
-    document.getElementById('services_categories_cards').style.display = "display";
-    document.getElementById('join_us_section').style.display = "display";
-    document.getElementById('footer_section').style.display = "display";
-    document.getElementById('Services_info_section').style.display = "display";
+    document.getElementById('fixit_inquiry_section').style.display = "block";
+    document.getElementById('services_categories_cards').style.display = "block";
+    document.getElementById('join_us_section').style.display = "block";
+    document.getElementById('footer_section').style.display = "block";
+    document.getElementById('Services_info_section').style.display = "block";
 });
 
 // event listener for category section to remove section
